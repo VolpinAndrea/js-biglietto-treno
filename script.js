197305a8d1779bd328014db18a8471e29dc695b8
VolpinAndrea <andvolpin19@gmail.com>
2022-11-10 17:21:52 +0100

    FINE html

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
-// let numerokm = document.getElementById("input-km").value;
-// let eta = document.getElementById("input-eta").value;   <>
+let startButton = document.getElementById("button");
 
-let numerokm = prompt("Km");
-let eta = prompt("eta");
+startButton.addEventListener('click', function(){
 
+//     let numerokm = prompt("Km");
+//     let eta = prompt("eta");<>
+
+let numerokm = document.getElementById("input-km").value;
+let eta = document.getElementById("input-eta").value;   
 
 console.log("Km: " + numerokm + " Età: "+ eta);
-
-
 
 let prezzo = numerokm * 0.21;
 
 console.log("Prezzo intero: "+ prezzo.toFixed(2));
 
-if (eta < 18){
+if (eta > 0 && eta < 18){
     prezzo -= (prezzo*20)/100;
-    console.log("giovane: "+ prezzo.toFixed(2));
+    console.log("Sconto giovani: "+ prezzo.toFixed(2));
 
-} else if(eta > 65){
+    document.getElementById("sconto").innerHTML = "Sconto giovani";
+    document.getElementById("prezzo").innerHTML = prezzo +" &euro;";
+
+} else if(eta > 65 && eta < 120){
     prezzo -= (prezzo*40)/100;
-    console.log("vecchio: "+ prezzo.toFixed(2));
+    console.log("Sconto anziani: "+ prezzo.toFixed(2)); 
+
+    document.getElementById("sconto").innerHTML = "Sconto anziani";
+    document.getElementById("prezzo").innerHTML = prezzo +" &euro;";
 
 
 }else if(eta >= 18 && eta <= 65){
     console.log("prezzo intero: "+ prezzo.toFixed(2));
+
+    document.getElementById("sconto").innerHTML = "Prezzo intero";
+    document.getElementById("prezzo").innerHTML = prezzo +" &euro;";
+
 
 }else {
     console.log("eta non valida");
 
 }
 
-if (isNaN(prezzo)||isNaN(eta)){
+if (isNaN(prezzo)||isNaN(eta)||numerokm<0){
     console.log("qualcosa è andato storto, ricaricare la pagina")
+    
 }
+})
+
